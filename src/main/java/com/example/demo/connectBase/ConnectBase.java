38866b3d0429d43ae1a03632849db6b271be0e4c
liantsoa17 <liantsurakotonirina@gmail.com>
2023-02-05 19:07:23 +0300

    mdp

--- src/main/java/com/example/demo/connectBase/ConnectBase.java
+++ src/main/java/com/example/demo/connectBase/ConnectBase.java
@@ -9,7 +9,7 @@ public class ConnectBase {
         Connection con = null;
         try{
             Class.forName("org.postgresql.Driver");
-            con  = DriverManager.getConnection("jdbc:postgresql://containers-us-west-126.railway.app:5844/railway" , "postgres" , "ToHCo0uzdQXyX5cjJgrI") ;
+            con  = DriverManager.getConnection("jdbc:postgresql://containers-us-west-133.railway.app:7816/railway" , "postgres" , "ZzrqsXOVSYprINJwIDYL") ;
             System.out.println("connecter");
         }
         catch(Exception e){
